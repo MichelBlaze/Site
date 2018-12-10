065e2723451b4c488f42e81a8528f4d623e39e0f
mimimama79 <45553886+mimimama79@users.noreply.github.com>
2018-12-10 11:57:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,59 +1,88 @@
-﻿<?php
+<?php
 $page= isset($_GET["page"]) ? $_GET["page"] : 1;
 ?>
 <html>
 <head>
 <link rel="stylesheet" href="css/style.css?<?php echo $time ?>">
 
+
 </head>
-<body>
+<body class=fond >
 
 
 <div id="menu">
+
 <ul>
 	<li>
 	<a <?php if($page==1){ ?>class="active"<?php } ?> href="index.php?page=1">home</a>
-
-	
-	
 	</li>
-
-	<li class="dropdown"><a <?php if($page==2){ ?>class="active"<?php } ?> href="index.php?page=2">Quizz</a>
+	<li class="dropdown"><a>Quizz</a>
 	<div class="dropdown-content">
-		<p><a href="index.php?page=1">Maths</a></p>
-		<p><a href="index.php?page=1">Anglais</a></p>
-		<p><a href="index.php?page=1">Physique</a></p>
-		<p><a href="index.php?page=1">...</a></p>
+		<p><a <?php if($page==21){ ?>class="active"<?php }?>href="index.php?page=21">Maths</a></p>
+		<p><a <?php if($page==22){ ?>class="active"<?php } ?>href="index.php?page=22">Anglais</a></p>
+		<p><a <?php if($page==23){ ?>class="active"<?php } ?>href="index.php?page=23">Physique</a></p>
+		<p><a <?php if($page==24){ ?>class="active"<?php } ?>href="index.php?page=24">...</a></p>
 
 		
 	</div>
 	</li>
-	<li class="dropdown"><a <?php if($page==3){ ?>class="active"<?php } ?> href="index.php?page=3">Niveaux</a>
+	<li class="dropdown"><a>Niveaux</a>
 	<div class="dropdown-content">
-		<p><a href="index.php?page=1">graph</a></p>
-		<p><a href="index.php?page=1">score</a></p>
-		<p><a href="index.php?page=1">graph</a></p>
+		<p><a <?php if($page==31){ ?>class="active"<?php } ?>href="index.php?page=31">graph</a></p>
+		<p><a <?php if($page==32){ ?>class="active"<?php } ?>href="index.php?page=32">score</a></p>
+		<p><a <?php if($page==33){ ?>class="active"<?php } ?>href="index.php?page=33">graph</a></p>
 
 
 	</div>
 	</li>
 </ul>
+
+<div class=ecrie>
+
+</div>
+<div class=connexion>
+	<ul>
+	<li><a <?php if($page==4){ ?>class="active"<?php } ?> href="index.php?page=4">Connexion</a></li>
+	<li><a <?php if($page==5){ ?>class="active"<?php } ?> href="index.php?page=5">Inscription</a></li>
+	</ul>
+	
+</div>
+</div>
 <?php 
 switch ($page) {
-		case 1 :
-			include("page1.php");
+		case 1:
+			include("Home.php");
 			break;
-		case 2 :
-			include("page2.php");
+		case 21 :
+			include("Maths.php");
 			break;
-		case 3 :
-			include("page3.php");
+		case 22 :
+			include("Anglais.php");
 			break;
+		case 23 :
+			include("Physique.php");
+			break;
+		case 31 :
+			include("Graph.php");
+			break;
+		case 32 :
+			include("Score.php");
+			break;
+		case 33 :
+			include("Graph.php");
+			break;
+		case 4 :
+			include("Connexion.php");
+			break;
+		case 5 :
+			include("Inscription.php");
+			break;
+
 }
 
 ?>
 
 
-</div>
+
 </body>
 </html>
